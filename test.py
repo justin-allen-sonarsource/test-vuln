5856699117c3091f5627bcd893d466b947f81210
justin-allen-sonarsource <59912466+justin-allen-sonarsource@users.noreply.github.com>
2021-11-16 15:17:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -35,4 +35,5 @@ requests.get('https://example.domain', verify=False) # Noncompliant
 
 requests.request('GET', 'http://example.domain', verify=False) # Noncompliant
 requests.get('http://example.domain', verify=False) # Noncompliant
-
+requests.request('GET', 'http://example.domain', verify=False) # Noncompliant
+requests.get('http://example.domain', verify=False) # Noncompliant
