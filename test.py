af63b8fadbf81f3a089f8e0d525a6d1a3ada5518
justin-allen-sonarsource <59912466+justin-allen-sonarsource@users.noreply.github.com>
2021-11-16 17:49:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -51,3 +51,29 @@ requests.get('https://example.domain', verify=False) # Noncompliant
 
 requests.request('GET', 'http://example.domain', verify=False) # Noncompliant
 requests.get('http://example.domain', verify=False) # Noncompliant
+
+
+
+requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
+requests.get('https://example.domain', verify=False) # Noncompliant
+
+
+requests.request('GET', 'http://example.domain', verify=False) # Noncompliant
+requests.get('http://example.domain', verify=False) # Noncompliant
+
+
+requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
+requests.get('https://example.domain', verify=False) # Noncompliant
+
+
+requests.request('GET', 'http://example.domain', verify=False) # Noncompliant
+requests.get('http://example.domain', verify=False) # Noncompliant
+
+
+requests.request('GET', 'https://example.domain', verify=False) # Noncompliant
+requests.get('https://example.domain', verify=False) # Noncompliant
+
+
+requests.request('GET', 'http://example.domain', verify=False) # Noncompliant
+requests.get('http://example.domain', verify=False) # Noncompliant
+
